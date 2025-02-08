8c16f605e8dbcf354c09c1192dcbc8f16dc2e26a
NewUsername <newemail@example.com>
2025-02-08 16:33:59 -0500

    Added password validation in views.py

--- backend/reels/views.py
+++ backend/reels/views.py
@@ -27,13 +27,17 @@ class RegisterAPI(generics.GenericAPIView):
     def validated_password (self, password):
 
         if not re.search(r"[A-Z]", password):
-            return "Password must contain at least one uppercase letter."
+            return Response({
+            "result": "Password must contain at least one uppercase letter."})
         if not re.search(r"[a-z]", password):
-            return "Password must contain at least one lowercase letter."
+            return Response({
+            "result": "Password must contain at least one lowercase letter."}) 
         if not re.search(r"[0-9]", password):
-            return "Password must contain at least one digit."
+            return Response({
+            "result":"Password must contain at least one digit."}) 
         if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
-            return "Password must contain at least one special character."
+            return Response({
+            "result":"Password must contain at least one special character."}) 
 
         return None
     
